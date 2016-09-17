03b767a65a87e1d5547ed84db04042c872d236ec
I-man <eadesina.ea@gmail.com>
2016-09-17 22:19:56 +1200

    Highlight selections
    Can highlight what you selected, also added color to the environment

---
+++ Assets/Player/Interact.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interact : MonoBehaviour {
+
+   
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+      
+      //  this.GetComponent<Renderer>().material.shader = Shader.Find("Standard");;
+        
+    }
+
+    public void OnLook()
+    {
+      
+        this.GetComponent<Renderer>().material.shader = Shader.Find("Custom/ImageEffectShader"); 
+        this.GetComponent<Renderer>().material.SetColor("_OutlineColor", Color.cyan);
+    }
+}
+
