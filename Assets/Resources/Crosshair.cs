533dc88a54ec9102ffafc84b92c229aab220c4d1
I-man <eadesina.ea@gmail.com>
2016-09-25 14:59:56 +1300

    Added the UI package
    Done

---
+++ Assets/Resources/Crosshair.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+    Rect crosshairRect;
+    Texture crosshairTexture;
+
+	// Use this for initialization
+	void Start () {
+        float crosshairSize = Screen.width * 0.025f;
+        crosshairTexture = (Texture)Resources.Load("Textures/crosshair1");
+        crosshairRect = new Rect(Screen.width / 2 - crosshairSize, Screen.height / 2 - crosshairSize / 2, crosshairSize, crosshairSize);
+    
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+        GUI.DrawTexture(crosshairRect, crosshairTexture);
+	}
+}
