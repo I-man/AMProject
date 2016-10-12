8a5f06ae852c6c505a3f99011c230293079cb03a
James Chen <jche846@aucklanduni.ac.nz>
2016-10-13 00:13:01 +1300

    transition
    half working

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,22 +1,72 @@
-﻿using UnityEngine;
+
+using UnityEngine;
 using System.Collections;
 
 public class MenuManager : MonoBehaviour {
 
-	public Menu CurrentMenu;
+     public int menuID=0;
+     public GameObject[] menuPanels;
+	 private GameObject welcomePanel;
+     private GameObject mainMenuPanel;
+     private GameObject tutorialPanel;
+     private GameObject schedulePanel;
+     private GameObject mapPanel;
+     private GameObject cameraPanel;
+	 private GameObject sharePanel;
 
-	public void Start()
-	{
-		ShowMenu (CurrentMenu);
-	}
+     // Use this for initialization
+     void Start () {
+         menuPanels = GameObject.FindGameObjectsWithTag("MenuPanel");
+		 welcomePanel=GameObject.Find("welcomePanel");
+         mainMenuPanel = GameObject.Find("MainMenuPanel");
+		 tutorialPanel = GameObject.Find("tutorialPanel");
+		 schedulePanel = GameObject.Find("schedulePanel");
+		 mapPanel = GameObject.Find("mapPanel");
+		 cameraPanel = GameObject.Find("cameraPanel");
+		 sharePanel = GameObject.Find("sharePanel");
 
-	public void ShowMenu(Menu menu)
-	{
-		if (CurrentMenu != null)
-			CurrentMenu.IsOpen = false;
 
-		CurrentMenu = menu;
-		CurrentMenu.IsOpen = true;
-	}
+ //        int playerNum = PlayerInfo.playerID;
+ //        Debug.Log (playerNum);
+         switchToMenu (menuID);
+     }
 
-}
+     // Update is called once per frame
+     void Update () {
+
+     }
+
+     public void switchToMenu(int menuID) {
+
+         foreach(GameObject panel in menuPanels)
+         {
+ //            panel.gameObject.renderer.enabled=false;
+             panel.gameObject.SetActive(false);
+             Debug.Log (panel.name);
+         }
+
+         switch (menuID) {
+             case 0:
+			welcomePanel.gameObject.SetActive(true);
+                 break;
+             case 1:
+			mainMenuPanel.gameObject.SetActive(true);
+                 break;
+             case 2:
+			tutorialPanel.gameObject.SetActive(true);
+                 break;
+             case 3:
+			schedulePanel.gameObject.SetActive(true);
+                 break;
+             case 4:
+			mapPanel.gameObject.SetActive(true);
+                 break;
+             case 5:
+			cameraPanel.gameObject.SetActive(true);
+                 break;
+			 case 6:
+			sharePanel.gameObject.SetActive(true);
+				break;
+         }
+     }
+ }
